101e8972e711b7ef4781cabd5109d1e29341c7f3
maimahran99 <96849339+maimahran99@users.noreply.github.com>
2022-11-07 17:37:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "font-awesome/css/font-awesome.min.css";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
